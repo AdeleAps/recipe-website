10e4af01164a022fc3933cb14ba00e3233b9af07
Adele Apsite <adeleapsite@gmail.com>
2024-03-14 22:42:58 +0200

    Adds an overlay with full recipe

--- Website/index.js
+++ Website/index.js
@@ -30,3 +30,33 @@ window.addEventListener(
   },
   false
 );
+
+function openRecipe(element) {
+  $("#recipeLightbox .modal-body").empty();
+
+  const card = $(element);
+  const recipeTitle = card.find(".card-body .card-text").text();
+  const image = card.find(".image-container .card-img-top").attr("src");
+
+  $("<img>", {
+    src: image,
+    class: "card-recipe-image",
+    alt: "Recipe Image",
+  }).appendTo("#recipeLightbox .modal-body");
+
+  // Append a dummy list of ingredients
+  $("<ul>", { class: "list-of-ingredients" })
+    .append("<li>Ingredient 1</li>")
+    .append("<li>Ingredient 2</li>")
+    .append("<li>Ingredient 3</li>")
+    .appendTo("#recipeLightbox .modal-body");
+
+  // Append a dummy recipe paragraph
+  $("<p>", {
+    class: "recipe-description",
+    text: "This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish. This is a dummy recipe description. Follow the steps to make your delicious dish.",
+  }).appendTo("#recipeLightbox .modal-body");
+
+  $("#recipeLightboxTitle").text(recipeTitle);
+  $("#recipeLightbox").modal("show");
+}
