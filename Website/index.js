b75140dfa708549aaa2dd2f3cf7f14b73ebeb02f
Adele Apsite <adeleapsite@gmail.com>
2024-03-16 12:06:03 +0200

    Sets up data fetching logic

--- Website/index.js
+++ Website/index.js
@@ -1,6 +1,14 @@
+let api;
+
 window.addEventListener("load", function () {
+  api = axios.create({
+    baseURL: "https://www.themealdb.com/api/json/v1/1/",
+  });
+
   handleFormSubmission();
   togglePlaceholderFade();
+  getAllMealCategories();
+  getAllRecipes();
 });
 
 function handleFormSubmission() {
@@ -73,3 +81,86 @@ function togglePlaceholderFade() {
     }
   );
 }
+
+const getAllMealCategories = async () => {
+  try {
+    const response = await api.get("list.php?a=list");
+    const mealCategories = response.data.meals.map((item) => item.strArea);
+    const dropdownMenu = $(".dropdown-menu");
+
+    if (dropdownMenu) {
+      dropdownMenu.empty();
+
+      if (mealCategories.length > 0) {
+        mealCategories.forEach((category) => {
+          dropdownMenu.append(
+            `<li onClick="handleSwitchCategory('${category}')" class="dropdown-item clickable" >${category}</li>`
+          );
+        });
+      } else {
+        dropdownMenu.append(
+          `<li class="dropdown-item"> No categories found. </li>`
+        );
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    const dropdownMenu = $(".dropdown-menu");
+    dropdownMenu.append(`<li class="dropdown-item"> Server error. </li>`);
+  }
+};
+
+const getAllRecipes = async (category = "American") => {
+  try {
+    showLoadingSpinner(); 
+    
+    const response = await api.get(`filter.php?a=${category}`);
+    const meals = response.data.meals.slice(0, 8);
+    const recipeGrid = $(".recipe-grid");
+
+    recipeGrid.empty();
+
+    if (meals.length > 0) {
+      meals.forEach((recipe) => {
+        const cardHtml = `
+          <div class="card card-homepage" onClick="openRecipe(this)">
+            <div class="image-container">
+              <img
+                class="card-img-top"
+                src="${recipe.strMealThumb}"
+                alt="${recipe.strMeal}"
+              />
+              <img
+                src="./Assets/heart.svg"
+                alt="Favorite"
+                class="heart-icon"
+              />
+              <div class="card-body">
+                <p class="card-text">${recipe.strMeal}</p>
+              </div>
+            </div>
+          </div>`;
+        recipeGrid.append(cardHtml); 
+      });
+    } else {
+      recipeGrid.append(`<p>No recipes found for ${category}.</p>`);
+    }
+  } catch (error) {
+    console.error(error);
+    $(".recipe-grid").html(`<p>Error loading recipes.</p>`);
+  }
+};
+
+const handleSwitchCategory = (category) => {
+  getAllRecipes(category);
+};
+
+const showLoadingSpinner = () => {
+  const spinnerHtml = `
+  <div class="loading-spinner-container">
+  <div id="loading-spinner">
+    <span class="sr-only">Loading...</span>
+  </div>
+  </div>`;
+  $(".recipe-grid").html(spinnerHtml);
+};
